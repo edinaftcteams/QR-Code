fee4903fab31a8275139f2a297ec8c01dd5b9c43
rhsftc <ronsearle@hotmail.com>
2022-06-15 16:50:15 -0500

    Refactoring

--- app/src/main/java/qr/code/GenerateQRCode.java
+++ app/src/main/java/qr/code/GenerateQRCode.java
@@ -1,6 +1,5 @@
 package qr.code;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import com.google.zxing.BarcodeFormat;
@@ -11,7 +10,7 @@ import com.google.zxing.common.BitMatrix;
 
 public class GenerateQRCode {
     // static function that creates QR Code
-    public static void generateQRcode(String data, String path, String charset, int h, int w)
+    public void generateCode(String data, String path, String charset, int h, int w)
             throws WriterException, IOException {
         // the BitMatrix class represents the 2D matrix of bits
         // MultiFormatWriter is a factory class that finds the appropriate Writer
@@ -24,27 +23,5 @@ public class GenerateQRCode {
         } catch (IOException e) {
             System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
         }
-    }
-
-    // main() method
-    public static void main(String args[]) throws WriterException, IOException {
-        // data that we want to store in the QR code
-        String str = "Edina FTC robots QR Codes.";
-        // path where we want to get QR Code
-        String path = "G:\\Windows Folders\\Documents\\github\\rhsftc\\QR Code\\codes\\qr.png";
-        // Encoding charset to be used
-        String charset = "UTF-8";
-        // generates QR code with Low level(L) error correction capability
-        // invoking the user-defined method that creates the QR code
-        generateQRcode(str, path, charset, 200, 200);// increase or decrease height and width accodingly
-        // prints if the QR code is generated
-        System.out.println("QR Code created successfully.");
-
-        File file = new File(path);
-
-        QRCodeReader qrCodeReader = new QRCodeReader();
-        String encodedContent = qrCodeReader.readQRCode(file);
-
-        System.out.println(encodedContent);
     }
 }

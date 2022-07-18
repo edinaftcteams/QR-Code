f77bc21e5592964d8ce113b49ffe2edeae8aabbe
rhsftc <ronsearle@hotmail.com>
2022-07-18 16:51:34 -0500

    This is now a VSCODE project.

--- app/src/main/java/qr/code/QRCode.java
+++ app/src/main/java/qr/code/QRCode.java
@@ -6,17 +6,18 @@ import com.google.zxing.WriterException;
 
 public class QRCode {
   // main() method
-  public static void main(String args[]) throws WriterException, IOException {
+  public static void main(String[] args) throws IOException {
     // data that we want to store in the QR code
     String str = "Edina FTC robots QR Codes.";
     // path where we want to get QR Code
-    String path = "G:\\Windows Folders\\Documents\\github\\rhsftc\\QR Code\\codes\\qr.png";
+//    String path = "G:\\Windows Folders\\Documents\\github\\rhsftc\\QR Code\\codes\\qr.png";
+    String path = "qr.png";
     // Encoding charset to be used
     String charset = "UTF-8";
     // invoking the user-defined method that creates the QR code
     try {
       GenerateQRCode generateQRcode = new GenerateQRCode();
-      generateQRcode.generateCode(str, path, charset, 200, 200);// increase or decrease height and width accodingly
+      generateQRcode.generateCode(str, path, charset, 200, 200);// increase or decrease height and width accordingly
       // prints if the QR code is generated
       System.out.println("QR Code created successfully.");
     } catch (WriterException ex) {
